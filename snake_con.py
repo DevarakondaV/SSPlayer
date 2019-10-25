4002736ea937e4b5a0d9d53fb8a200af10139fdb
devarakondaV <devarakonda.vishnu5@gmail.com>
2019-10-24 21:23:01 -0700

    Referenced added for sumTree,exp. Changes for game size made

--- snake_con.py
+++ snake_con.py
@@ -28,7 +28,7 @@ class snake:
         self.chrome_path = r'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
         if id==1:
             self.ext_path = r"C:\Users\devar\AppData\Local\Google\Chrome\User Data\Default\Extensions\gllcngkdngnfgilfmcbaanknakfgfepb\4.0_0"
-            self.processing_crop_100 = {'left':128,
+            self.processing_crop = {'left':128,
                                     'top': 582,
                                     'width': 201,
                                     'height': 200}
@@ -36,7 +36,7 @@ class snake:
                                     'top': 582,
                                     'width': 301,
                                     'height': 302}
-            self.processing_crop = {'left':128,
+            self.processing_crop_200 = {'left':128,
                                     'top': 477,
                                     'width': 402,
                                     'height': 402}
@@ -92,9 +92,9 @@ class snake:
 
         self.canvas = chrome.find_element_by_id("snake-game")
         self.game_container = chrome.find_element_by_class_name("container")
-        self.chrome.execute_script("arguments[0].setAttribute('width','200')", self.canvas)
-        self.chrome.execute_script("arguments[0].setAttribute('height','200')", self.canvas)
-        self.chrome.execute_script("arguments[0].setAttribute('style','width: 200px;')", self.game_container)
+        self.chrome.execute_script("arguments[0].setAttribute('width','100')", self.canvas)
+        self.chrome.execute_script("arguments[0].setAttribute('height','100')", self.canvas)
+        self.chrome.execute_script("arguments[0].setAttribute('style','width: 100px;')", self.game_container)
         self.chrome.execute_script("arguments[0].setAttribute('class','')", self.game_container)
         
                 
